d9614855ba0ac1861f8155cea61d506cec72fb47
James Pearce <github@tripleodeon.com>
2011-11-30 11:09:56 -0800

    2_ui_structure

--- app/app.js
+++ app/app.js
@@ -3,9 +3,38 @@ Ext.application({
 
     launch: function() {
         CB = this;
-        Ext.create('Ext.Panel', {
+        CB.cards = Ext.create('Ext.Panel', {
             fullscreen: true,
-            html: 'Hello world'
+            layout: 'card',
+            items: [{
+                // the list card
+                id: 'listCard',
+                layout: 'fit',
+                items: [{
+                    // main top toolbar
+                    xtype: 'toolbar',
+                    docked: 'top',
+                    title: 'Please wait'
+                    // will get added once city known
+                }, {
+                    // the list itself
+                    // gets bound to the store once city known
+                    id: 'dataList',
+                    xtype: 'list',
+                    store: null
+                }]
+            }, {
+                // the details card
+                id: 'detailCard',
+                items: [{
+                    // detail page also has a toolbar
+                    docked : 'top',
+                    xtype: 'toolbar',
+                    title: ''
+                }, {
+                    // textual detail
+                }]
+            }]
         });
     }
 
